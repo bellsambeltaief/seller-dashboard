719573fe9f7b05047bb5d72277fa0f756c7bcb0c
Bellsam Beltaief <86749375+bellsambeltaief@users.noreply.github.com>
2022-04-19 01:31:17 +0100

    first commit

---
+++ images/orders.js
@@ -0,0 +1,37 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatuts: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'LAVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatuts: 'Refunded',
+        shipping: 'Declined'
+
+    },
+    { 
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatuts: 'Due',
+        shipping: 'Pending'
+
+
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatuts: 'Paid',
+        shipping: 'Delivered'
+
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatuts: 'Paid',
+        shipping: 'Delivered'
+
+    }
+]
